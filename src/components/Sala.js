011213f711f7078fb15fc0281b6ae3750c72b311
existro <existrox@gmail.com>
2021-02-26 14:57:30 -0600

    estilo

--- src/components/Sala.js
+++ src/components/Sala.js
@@ -5,7 +5,7 @@ const Sala = (props)=>{
     return(
         <div className="container align-items-center d-md-flex justify-content-evently h-100">
             <div>
-                <div className="display-3 text-light animate__animated animate__backInLeft">Cartelera</div>
+                <div className="display-3 text-light">Cartelera</div>
                 <Galery className="card-group"></Galery> 
             </div>
             </div>
